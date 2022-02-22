fdd7633d0843f1364fd0b73431006148aafc37b2
macbook <khaitruong1301@gmail.com>
2022-02-22 21:03:42 +0700

    devb_f2b

--- app/method/featureA.js
+++ app/method/featureA.js
@@ -2,5 +2,4 @@ function featureA () {
     console.log('dev A code');
     console.log('dev A tự fix bug');
     console.log('dev B fix bug');
-
 }
--- app/method/featureB.js
+++ app/method/featureB.js
@@ -1,3 +1,7 @@
 function featureB(){
     console.log('dev B code');
 }
+
+function feature2B() {
+    console.log('ahihihi');
+}
